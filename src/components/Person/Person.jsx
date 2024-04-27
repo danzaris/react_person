de5b7036f9cedb3796579bedd5c63228fc3246b8
Denys Povh <kirilloffdanza@gmail.com>
2024-04-27 22:07:30 +0300

    upgrade code

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const Person = ({ person }) => {
-  const partnerLabel = person.sex === 'm' ? 'wife' : 'husband';
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const partnerLabel = sex === 'm' ? 'wife' : 'husband';
+
+  const partnerContent = isMarried
+    ? `${partnerName} is my ${partnerLabel}`
+    : 'I am not married';
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age && <p className="Person__age">I am {person.age}</p>}
-      {person.isMarried ? (
-        <p className="Person__partner">
-          {person.partnerName} is my {partnerLabel}
-        </p>
-      ) : (
-        <p className="Person__partner">I am not married</p>
-      )}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerContent}</p>
     </section>
   );
 };
